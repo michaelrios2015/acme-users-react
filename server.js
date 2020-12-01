83e51777cfa9ea9ef12fcfd4a6e47c0cd6b0747d
Michael Rios <michael.rios093@gmail.com>
2020-11-30 19:11:29 -0500

    finished??

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
 
---
+++ src/User.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class User extends Component{
+    constructor(){
+        super()
+        this.state = {
+            user: {}
+        };
+    }
+    async componentDidUpdate(prevProps){
+        if (prevProps.selectedUserId != this.props.selectedUserId){
+            const user = (await axios.get(`/api/users/${this.props.selectedUserId}`)).data;
+            this.setState({ user })
+            //console.log(this.props);
+        }
+    }
+    async componentDidMount(){
+        const user = (await axios.get(`/api/users/${this.props.selectedUserId}`)).data;
+        this.setState({ user })
+        //console.log(user);
+    }
+    render(){
+        const { user } = this.state;
+        return (
+            <div>
+                { user.bio }
+            </div>
+        );
+    }
+}
+
+export default User;
